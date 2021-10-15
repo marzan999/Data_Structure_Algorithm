ba87061b8c1bcf750b7311b6fb220a23bc8e0385
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2021-10-15 19:08:12 +0600

    Add files via upload

---
+++ bubble sort.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, i, j;
+
+    cout<<"How many numbers you want: ";
+    cin>>n;
+
+    int list[n];
+
+    cout<<"Enter the numbers: "<<endl;
+    for(i=0; i<n; i++)
+    {
+        printf("Index[%d", i);
+        cout<<"]= ";
+        cin>>list[i];
+    }
+    cout<<endl;
+
+    for(i=0; i<=n-1; i++)
+    {
+
+        for(j=0; j<n-i-1; j++)
+        {
+            int temp;
+            if(list[j]>list[j+1])
+
+            {
+                temp=list[j];
+                list[j]=list[j+1];
+                list[j+1]=temp;
+            }
+        }
+    }
+    cout<<endl<<"sorted list"<<endl;
+    for(i=0; i<n; i++)
+    {
+        cout<<" "<<list[i];
+    }
+    return 0;
+}
